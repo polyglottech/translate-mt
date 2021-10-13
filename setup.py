3bbb73d30d577e2ed7af353498b96a0ecb816b86
Achim Ruopp <achim@polyglot.technology>
2021-10-12 21:30:20 -0400

    Initial library test setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='translate-mt',
+      version='0.0.1',
+      description='Library to access different MT services in a unified way',
+      url='https://github.com/polyglottech/translate-mt',
+      author='Achim Ruopp',
+      author_email='achim@polyglot.technology',
+      license='MIT',
+      packages=['translate-mt'],
+      zip_safe=False)
---
+++ translate-mt/__init__.py
@@ -0,0 +1,2 @@
+def test_method():
+    return "Test message from the translate-mt library"
